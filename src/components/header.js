03f6c75ef79329d5e9d411627ba97363ba9b03d0
[thecubix] <[timon.forrer@gmail.com]>
2018-12-18 12:01:45 +0100

    added links

--- src/components/header.js
+++ src/components/header.js
@@ -4,9 +4,9 @@ const Header = (title) => html`
 <header class="header is-fixed">
   <h1 class="header-title">${title}</h1>
   <ul class="header-list">
-    <li><a href="/">Home</a></li>
-    <li><a href="/todos/">Todos</a></li>
-    <li><a href="/about/">about</a></li>
+    <li><a href="/#/">Home</a></li>
+    <li><a href="/#/todos/">Todos</a></li>
+    <li><a href="/#/about/">about</a></li>
   </ul>
 </header>
 `
