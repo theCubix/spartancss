7609048e338782e2be87676fa5b40708f5a3066d
[thecubix] <[timon.forrer@gmail.com]>
2018-12-18 11:26:01 +0100

    Lists don't have a colour by default

--- src/components/hero-section.js
+++ src/components/hero-section.js
@@ -4,7 +4,7 @@ const heroSection = () => html`
   <section class="section is-fullheight is-center">
     <div class="container">
       <h1 class="title is-1 has-margin">SpartanCSS.io</h1>
-      <ul>
+      <ul class="list">
         <li>a css collection</li>
         <li>reduced to the minimum</li>
         <li>with little <span class="underlined">sparks of colour</span></li>
